7e667c18441a43efee9b42f7f2c8c0d7ee10183d
João Simões <nogravity00@outlook.com>
2018-01-01 20:50:41 +0000

    Entity interface

---
+++ SimpleSoft.Persistence.Model/SimpleSoft.Persistence.Model.Abstractions/IEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleSoft.Persistence.Model
+{
+    /// <summary>
+    /// Represents an entity with an unique identifier
+    /// </summary>
+    /// <typeparam name="TId">The unique identifier type</typeparam>
+    public interface IEntity<TId> : IEntity
+    {
+        /// <summary>
+        /// The entity unique identifier
+        /// </summary>
+        TId Id { get; set; }
+    }
+
+    /// <summary>
+    /// Represents an entity
+    /// </summary>
+    public interface IEntity
+    {
+
+    }
+}
