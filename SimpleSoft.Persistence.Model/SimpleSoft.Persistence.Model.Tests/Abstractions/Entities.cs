1c6815ad3028497bd98301c767eef4a126a06ec9
João Simões <nogravity00@outlook.com>
2018-01-07 16:09:26 +0000

    Added mock entities

---
+++ SimpleSoft.Persistence.Model/SimpleSoft.Persistence.Model.Tests/Abstractions/Entities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SimpleSoft.Persistence.Model.Tests.Abstractions
+{
+    public class EntityStringId : Entity<string>
+    {
+        public EntityStringId() { }
+
+        public EntityStringId(string id) : base(id) { }
+    }
+
+    public class EntityIntId : Entity<int>
+    {
+        public EntityIntId() { }
+
+        public EntityIntId(int id) : base(id) { }
+    }
+
+    public class EntityLongId : Entity<long>
+    {
+        public EntityLongId() { }
+
+        public EntityLongId(long id) : base(id) { }
+    }
+
+    public class EntityGuidId : Entity<Guid>
+    {
+        public EntityGuidId() { }
+
+        public EntityGuidId(Guid id) : base(id) { }
+    }
+}
